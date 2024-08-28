799916e1ba1b634e22ffda26ee82afb482525a83
Igor Alves <igor193@live.com>
2024-08-28 11:48:05 -0300

    fix: any

--- src/modules/chat/chatVIew.tsx
+++ src/modules/chat/chatVIew.tsx
@@ -1,54 +1,59 @@
-// src/components/ChatView.tsx
-
-import React, { ChangeEvent, MouseEvent } from "react";
+import React, { ChangeEvent, MouseEvent, useEffect, useState } from "react";
 import { ChatPresenter } from "./chatPresenter";
-import { ChatContainer, Container } from './style'
+import { ChatContainer, Container } from './style';
 
 interface ChatViewProps {
   presenter: ChatPresenter;
 }
 
-export default class ChatView extends React.Component<ChatViewProps> {
-  constructor(props: ChatViewProps) {
-    super(props);
-    this.props.presenter.forceUpdate = this.forceUpdate.bind(this);
-  }
+const ChatView: React.FC<ChatViewProps> = ({ presenter }) => {
+  const [input, setInput] = useState(presenter.getInput());
 
-  handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const input = e.target.value;
-    this.props.presenter.handleInputChange(input);
+  useEffect(() => {
+    presenter.forceUpdate = () => {
+      setInput(presenter.getInput());
+    };
+
+    return () => {
+      presenter.forceUpdate = () => {};
+    };
+  }, [presenter]);
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const newInput = e.target.value;
+    setInput(newInput);
+    presenter.handleInputChange(newInput);
   };
 
-  handleSend = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleSend = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    this.props.presenter.handleSend();
+    presenter.handleSend();
+    setInput('');
   };
 
-  render() {
-    const { presenter } = this.props;
-    const messages = presenter.getMessages();
-    const input = presenter.getInput();
+  const messages = presenter.getMessages();
 
-    return (
-      <Container>
-        <h1>Chat</h1>
-        <ChatContainer>
-          {messages.map((msg, index) => (
-            <p key={index} style={{ margin: "5px 0" }}>
-              {msg}
-            </p>
-          ))}
-        </ChatContainer>
-        <div style={{ display: "flex" }}>
-          <input
-            type="text"
-            value={input}
-            onChange={this.handleInputChange}
-            style={{ flex: 1, marginRight: "10px" }}
-          />
-          <button onClick={this.handleSend}>Send</button>
-        </div>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <h1>Chat</h1>
+      <ChatContainer>
+        {messages.map((msg, index) => (
+          <p key={index} style={{ margin: "5px 0" }}>
+            {msg}
+          </p>
+        ))}
+      </ChatContainer>
+      <div style={{ display: "flex" }}>
+        <input
+          type="text"
+          value={input}
+          onChange={handleInputChange}
+          style={{ flex: 1, marginRight: "10px" }}
+        />
+        <button onClick={handleSend}>Send</button>
+      </div>
+    </Container>
+  );
+};
+
+export default ChatView;
--- src/modules/counter/CounterView.tsx
+++ src/modules/counter/CounterView.tsx
@@ -1,32 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CounterPresenter } from "./counterPresenter";
 
 type CounterViewProps = {
   presenter: CounterPresenter;
 };
 
-class CounterView extends React.Component<CounterViewProps> {
-  constructor(props: CounterViewProps) {
-    super(props);
-    this.props.presenter.forceUpdate = this.forceUpdate.bind(this);
-  }
+const CounterView: React.FC<CounterViewProps> = ({ presenter }) => {
+  const [count, setCount] = useState(presenter.scope.count);
 
-  render() {
-    const { count } = this.props.presenter.scope;
+  useEffect(() => {
+    presenter.forceUpdate = () => {
+      setCount(presenter.scope.count);
+    };
 
-    return (
-      <div>
-        <h2>Counter View</h2>
-        <p>Count: {count}</p>
-        <button onClick={() => this.props.presenter.increment()}>
-          Increment
-        </button>
-        <button onClick={() => this.props.presenter.decrement()}>
-          Decrement
-        </button>
-      </div>
-    );
-  }
-}
+    return () => {
+      presenter.forceUpdate = () => {};
+    };
+  }, [presenter]);
+
+  const handleIncrement = () => {
+    presenter.increment();
+  };
+
+  const handleDecrement = () => {
+    presenter.decrement();
+  };
+
+  return (
+    <div>
+      <h2>Counter View</h2>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  );
+};
 
 export default CounterView;
